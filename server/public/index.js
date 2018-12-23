87a957ec804fd0cae7bcd8aef20447ea56eeaa35
javierc22 <javier.ec.22@gmail.com>
2018-12-23 16:13:38 -0300

    Mostrando temperatura en tiempo real por index.html

--- server/public/index.js
+++ server/public/index.js
@@ -1,6 +1,9 @@
 const socket = io();
 
+const temperatureDisplay = document.getElementById('temperature');
+
 // Mostrar lectura en consola de navegador
 socket.on('temp', function (data) {
   console.log(data);
+  temperature.innerHTML = `${data}°C`;
 });
