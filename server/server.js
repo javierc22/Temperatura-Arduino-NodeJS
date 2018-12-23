119d6e5de59a674aae6d8ff38e4f4b7632b68c0c
javierc22 <javier.ec.22@gmail.com>
2018-12-23 15:21:12 -0300

    Mostrando lectura a través de monitor serial

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const Serialport = require('serialport');
+
+// Obtener cada lectura en una línea nueva
+const Readline = Serialport.parsers.Readline;
+
+// Inicializar puerto serial
+const port = new Serialport("/dev/ttyACM0", {
+  baudRate: 9600
+});
+
+// Inicializar lectura
+const parser = port.pipe(new Readline( {delimeter: '\r\n'} ));
+
+// Cuando conexíon de puerto serie esté abierta
+parser.on('open', function() {
+  console.log('connection is open');
+});
+
+// Cuando conexión está recibiendo datos
+parser.on('data', function(data) {
+  console.log(data);
+  // console.log(typeof data); => String
+});
+
+// Cuando conexión tenga un error
+parser.on('error', function() {
+  console.log(err);
+});
